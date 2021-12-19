6e61c6e0a15721a9cb7c4494e138159d88e17f7a
saloni kumawat <545saloni@gmail.com>
2021-12-19 18:40:51 +0100

    Home component for logged in user

--- client/db/users.js
+++ client/db/users.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
+export const getUser = () => {
+    return Meteor.user();
+}
+
 export const login = (username, password) => {
     Meteor.loginWithPassword(username, password);
 }
--- client/ui/App.jsx
+++ client/ui/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { LoginForm } from './LoginForm';
+import { Home } from './Home';
+import { getUser } from '../db/users';
+import { useTracker } from 'meteor/react-meteor-data'
 
 export const App = () => {
+    const user = useTracker(() => getUser());
+
     return (
-        <LoginForm/>
+        <div className="main">
+            {user ? (<Home />) : (<LoginForm />)}
+        </div>
     );
 }
